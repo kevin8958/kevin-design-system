928da89be5c4f6734a30b7de688e017b1db7cc24
kevin8958 <kevin8958@gmail.com>
2026-03-11 17:37:17 -0700

    fix: fix build error

---
+++ src/components/foundation/Colors.stories.tsx
@@ -0,0 +1,8 @@
+import ComponenColorsPage from '@/pages/components/foundation/colors/page';
+
+export default {
+  title: 'Foundation/Colors',
+  component: ComponenColorsPage,
+};
+
+export const Palette = () => <ComponenColorsPage />;
